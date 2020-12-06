4e3be2bb913f965b30bcb47c6d1d192bf734633c
Irina <irina@lanl.gov>
2020-12-06 16:25:50 -0700

    adding logic for per element swap

--- reverse_bytes.cc
+++ reverse_bytes.cc
@@ -2,14 +2,43 @@
 #include <fstream>
 #include <cstring> // for std::strlen
 #include <cstddef>  // for std::size_t -> is a typedef on an unsinged int
+#include <climits>
+#include <vector>
+
+template <typename T>
+T swap( T u){
+  static_assert (CHAR_BIT==8, " CHAR BIT not equalt to 8");
+
+ union
+ {
+   T u;
+   unsigned char u8[sizeof(T)];
+ } src, dst;
+
+  src.u = u;
+  size_t size = sizeof(T);
+
+  for (size_t i = 0; i < size; i++)
+    dst.u8[i] = src.u8[size -1 -i];
+ 
+  return dst.u;
+
+}
 
 using namespace std;
+
+
+
 int main ()
 {
+
+
   string filename = "input";
   ifstream file;
 	size_t size = 0; 
 
+#if 0
+  //swap all bytes in the file:
 	file.open( "test.txt", ios::in|ios::binary );
 	char* data = 0;
 
@@ -19,21 +48,32 @@ int main ()
   data = new char[ size+1];
 
   file.read( data, size );
-  data[size]='/0';
+  data[size]='\0';
 
-cout << "size = " <<size<<std::endl;
-
-  for (size_t i=0; i<size/2; i++)
+  for (size_t i=0; i<=size/2; i++)
   {
 
-std::cout <<" i = " << i << "last = "<< size-i-1<<std::endl;
-std::cout <<  data[i] <<" , " <<data[size-i-1]<<std::endl;
     char tmp = data[size-i-1];
     data[size-i-1]=data[i];
     data[i]=tmp;
   }
 
   file.close();
+
+#else
+    //swap bytes for each entry in a file
+    file.open( "test.txt", ios::in);
+    vector<int> data;
+
+    int tmp;
+    while (file >>tmp){
+      int tmp_s =swap<int>(tmp);
+      data.push_back(tmp_s);
+    }
+
+    file.close();
+
+#endif
 
   return 1;
 }
